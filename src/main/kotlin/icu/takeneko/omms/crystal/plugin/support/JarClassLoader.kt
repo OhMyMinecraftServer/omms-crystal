0f2e323ec73ca4b9ec9bfebf6d1d9f26d43c7ceb
ZhuRuoLing <jkl-963@live.com>
2025-07-22 00:21:05 +0800

    把classloader日的一声搅成糊糊

--- src/main/kotlin/icu/takeneko/omms/crystal/plugin/support/JarClassLoader.kt
+++ src/main/kotlin/icu/takeneko/omms/crystal/plugin/support/JarClassLoader.kt
@@ -1,5 +1,8 @@
 package icu.takeneko.omms.crystal.plugin.support
 
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.tree.AnnotationNode
+import org.objectweb.asm.tree.ClassNode
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.net.URLClassLoader
@@ -13,14 +16,47 @@ class JarClassLoader(parent: ClassLoader) : URLClassLoader(arrayOf(), parent) {
         this.addURL(file.toURI().toURL())
     }
 
-    fun scanClasses(file: File): ScanData {
+    fun scanClasses(file: File): List<ScanData> {
+        val scanDatas = mutableListOf<ScanData>()
         ZipFile(file).use {
             for (entry in it.entries().asSequence()) {
                 if (entry.name.endsWith(".class")) {
-
+                    val bytes = it.getInputStream(entry).readBytes()
+                    val classNode = ClassNode().apply {
+                        ClassReader(bytes).accept(this@apply, 0)
+                    }
+                    scanDatas += ScanData(
+                        entry.name.replace("/", "."),
+                        classNode.visibleAnnotations.map {
+                            AnnotationData(
+                                it.desc,
+                                it.values.sliceAnnotationEntries()
+                            )
+                        },
+                        classNode.methods.associate {
+                            MethodData(
+                                it.access,
+                                classNode.name,
+                                it.name,
+                                it.desc
+                            ) to it.visibleAnnotations.map {
+                                AnnotationData(
+                                    it.desc,
+                                    it.values.sliceAnnotationEntries()
+                                )
+                            }
+                        }
+                    )
                 }
             }
         }
-        return ScanData()
+        return scanDatas
+    }
+
+    private fun List<Any>?.sliceAnnotationEntries(): Map<String, Any> {
+        if (this == null) return mapOf()
+        return this.chunked(2).associate {
+            it[0] as String to it[1]
+        }
     }
 }
--- src/main/kotlin/icu/takeneko/omms/crystal/plugin/support/ScanData.kt
+++ src/main/kotlin/icu/takeneko/omms/crystal/plugin/support/ScanData.kt
@@ -1,4 +1,19 @@
 package icu.takeneko.omms.crystal.plugin.support
 
-class ScanData {
-}
+data class ScanData(
+    val classFqcn: String,
+    val classAnnotations: List<AnnotationData>,
+    val methodAnnotations: Map<MethodData, List<AnnotationData>>
+)
+
+data class MethodData(
+    val modifier: Int,
+    val owner: String,
+    val name: String,
+    val desc: String
+)
+
+data class AnnotationData(
+    val desc: String,
+    val values: Map<String, Any>
+)
